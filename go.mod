537a31d1ead416318669d900dae85355c50c50e1
zixiaohuang <zixiaohuangcn@gmail.com>
2022-03-13 21:50:09 +0800

    lesson3

--- go.mod
+++ go.mod
@@ -15,4 +15,6 @@ require (
 	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf
 	google.golang.org/grpc v1.44.0
 	google.golang.org/protobuf v1.27.1
+	gorm.io/driver/mysql v1.3.2 // indirect
+	gorm.io/gorm v1.23.2 // indirect
 )
--- internal/data/data.go
+++ internal/data/data.go
@@ -3,21 +3,35 @@ package data
 import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"kratos-realworld/internal/conf"
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
+var ProviderSet = wire.NewSet(NewDB, NewData, NewGreeterRepo)
 
 // Data .
 type Data struct {
-	// TODO wrapped database client
+	db *gorm.DB
 }
 
 // NewData .
-func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
+func NewData(c *conf.Data, logger log.Logger, db *gorm.DB) (*Data, func(), error) {
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
 	return &Data{}, cleanup, nil
+}
+
+func NewDB(c *conf.Data) *gorm.DB {
+	db, err := gorm.Open(mysql.Open(c.Database.Dsn), &gorm.Config{})
+	if err != nil{
+		panic("failed to connect database")
+	}
+	// 不建议生产环境用AutoMigrate
+	if err := db.AutoMigrate(); err != nil {
+		panic(err)
+	}
+	return db
 }
